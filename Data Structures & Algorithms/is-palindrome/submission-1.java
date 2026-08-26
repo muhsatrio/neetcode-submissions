ae62b95adc4cbda81fd9050d072119c9729c43cb
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2026-08-27 00:29:22 +0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,17 @@
+// TWO POINTER
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("\\s+", "");
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        int left = 0, right = s.length() - 1;
+
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            left++;
+            right--;
+        }
+
+        return left >= right;
+    }
+}
