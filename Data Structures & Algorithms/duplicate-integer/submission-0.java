36d45fd0ed67d5840ecd38186fcad7b7a85ce860
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2026-08-27 01:42:28 +0700

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> maps = new HashMap<>();
+
+        for (Integer num : nums) {
+            maps.put(num, maps.getOrDefault(num, 0) + 1);
+        }
+
+        for (Integer value : maps.values()) {
+            if (value > 1) return true;
+        }
+
+        return false;
+    }
+}
