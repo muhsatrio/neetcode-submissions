64763209b4de47d2111ade502ebd540877a7de1d
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2026-08-27 00:27:46 +0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.java
@@ -0,0 +1,28 @@
+// SLIDING WINDOW
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> maps = new HashMap<>();
+
+        int left = 0;
+
+        int maxFreq = 0;
+
+        int result = 0;
+
+        for (int right=0; right < s.length(); right++) {
+            maps.put(s.charAt(right), maps.getOrDefault(s.charAt(right), 0) + 1);
+
+            maxFreq = Math.max(maxFreq, maps.get(s.charAt(right)));
+
+            if ((right-left+1) - maxFreq > k) {
+                maps.put(s.charAt(left), maps.get(s.charAt(left)) - 1);
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+    }
+}
