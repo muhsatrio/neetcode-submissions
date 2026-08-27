9b321f70ad5e012e66d7304293146f3a6fe3d353
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2026-08-27 20:50:23 +0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,37 @@
+// Input: strs = ["Hello","World"]
+
+// Output: ["Hello","World"]
+
+class Solution {
+
+    public String encode(List<String> strs) {
+        String result = "";
+
+        for (String str : strs) {
+            result+=str.length() + "#" + str;
+        }
+
+        return result;
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < str.length()) {
+            int hashIndex = str.indexOf('#', i);
+
+            int length = Integer.parseInt(str.substring(i, hashIndex));
+
+            int start = hashIndex + 1;
+
+            String subs = str.substring(start, start+length);
+
+            result.add(subs);
+
+            i = start + length;
+        }
+        return result;
+    }
+}
