a1165265f0523b8d1d7ec9ae5d7283b10bc6f57f
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2026-08-27 20:51:57 +0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,21 @@
+// Input: s = "racecar", t = "carrace"
+
+// Output: true
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] splittedS = s.toCharArray();
+        char[] splittedT = t.toCharArray();
+
+        Arrays.sort(splittedS);
+        Arrays.sort(splittedT);
+
+        for (int i=0; i<splittedS.length; i++) {
+            if (splittedS[i] != splittedT[i]) return false;
+        }
+
+        return true;
+    }
+}
