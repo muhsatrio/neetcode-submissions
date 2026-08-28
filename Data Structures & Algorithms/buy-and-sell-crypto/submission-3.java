afac5290ab8771a1eeb5ee5992907dac3037e5c7
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2026-08-28 13:28:48 +0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,28 @@
+// SLIDING WINDOW
+
+// Input: prices = [10,1,5,6,7,1]
+
+// Output: 6
+
+// Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+
+        int result = 0;
+
+        for (int right=0; right<prices.length; right++) {
+            int currSum = prices[right] - prices[left];
+
+            if (prices[right] > prices[left]) {
+                int currProfit = prices[right] - prices[left];
+                result = Math.max(result, currProfit);
+            } else {
+                left = right;
+            }
+        }
+
+        return result;
+    }
+}
